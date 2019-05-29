a8f00bddb5f5f79a5db782cc59db423032dbc28b
murphywuwu <murphywuwu@gmail.com>
2019-05-29 17:27:52 +0800

    🚧 (Demo-01-Basic-routing) react router shared layout with render props

--- Demo-01-Basic-routing/src/App.js
+++ Demo-01-Basic-routing/src/App.js
@@ -3,59 +3,28 @@ import { Link, Route,Switch, Redirect } from 'react-router-dom';
 import ShowTheLocation from './ShowTheLocation';
 import { renderRoutes } from 'react-router-config'
 
-const Root = ({ route }) => (
-  <div>
-    <h1>Root</h1>
-    {/* child routes won't render without this */}
-    {renderRoutes(route.routes)}
-  </div>
-);
+function Layout1 (props) {
+return (<div>layout1{props.children}</div>)
+}
 
-const Home = ({ route }) => (
-  <div>
-    <h2>Home</h2>
-  </div>
-);
+const App = () => {
 
-const Child = ({ route }) => (
-  <div>
-    <h2>Child</h2>
-    {/* child routes won't render without this */}
-    {renderRoutes(route.routes, { someProp: "these extra props are optional" })}
-  </div>
-);
-
-const GrandChild = ({ someProp }) => (
-  <div>
-    <h3>Grand Child</h3>
-    <div>{someProp}</div>
-  </div>
-);
-
-const routes = [
-  {
-    component: Root,
-    routes: [
-      {
-        path: '/',
-        exact: true,
-        component: Home,
-      },
-      {
-        path: '/child/:id',
-        component: Child,
-        routes: [
-          {
-            path: '/child/:id/grand-child',
-            component: GrandChild
-          }
-        ]
-      }
-    ]
-  }
-]
-
-const App = renderRoutes(routes);
+  return (
+    <Switch>
+      <Route path="/" render={() => {
+        return (
+          <Layout1>
+            <Switch>
+             <Route path="/a" component={() => (<div>1</div>)}/>
+             <Route path="/b" component={() => (<div>2</div>)}/>
+            </Switch>
+          </Layout1>
+        )
+      }}>
+      </Route>
+    </Switch>
+  )
+}
 
 
 export default App;
