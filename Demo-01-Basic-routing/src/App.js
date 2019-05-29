d74aa1b272c41e0a5c2d5a4b697e072113ce04d1
murphywuwu <murphywuwu@gmail.com>
2019-05-29 18:44:15 +0800

    🚧 (Demo-01-Basic-routing) withRouter compare with pass  props

--- Demo-01-Basic-routing/src/App.js
+++ Demo-01-Basic-routing/src/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
-import { Link, Route,Switch, Redirect } from 'react-router-dom';
+import { Link, Route,Switch, Redirect, withRouter } from 'react-router-dom';
 import ShowTheLocation from './ShowTheLocation';
 import { renderRoutes } from 'react-router-config'
 
 function Layout1 (props) {
-return (<div>layout1{props.children}</div>)
+  console.log(props);
+  return (<div>layout1{props.children}</div>)
 }
+// 无论是用withRouter还是直接传递props，组件难道的值中的props.match.path都等于/
+// const Layout1 = withRouter(Layout); 
 
 const App = () => {
 
   return (
     <Switch>
-      <Route path="/" render={() => {
+      <Route path="/" render={(props) => {
         return (
-          <Layout1>
+          <Layout1 {...props}>
             <Switch>
              <Route path="/a" component={() => (<div>1</div>)}/>
              <Route path="/b" component={() => (<div>2</div>)}/>
